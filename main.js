63065ad4b3619e8ae0fd7367f01f9c9ddca48b32
vitoxint <vitoxint@gmail.com>
2023-04-30 16:48:32 -0400

    optimiza funciones , add jquery mejora diseño

--- main.js
+++ main.js
@@ -5,12 +5,38 @@ function addCharacter(character) {
     document.getElementById('result').innerHTML = result;
 }
 
-function calculate() {
+
+$("#calculate").click(function(){
+
+    var final = result.substring(result.length -1 );
+
+    if( isNaN( parseInt(final))){
+        return 0;
+    }
+
+
     result = eval(result);
     document.getElementById('result').innerHTML = result;
-}
 
-function clearResult() {
+});
+
+$(".actual").click(function(){
+
+    result += this.value;
+    document.getElementById('result').innerHTML = result;
+    
+
+});
+
+
+$("#clear").click( function() {
     result = '';
     document.getElementById('result').innerHTML = '';
-}
+});
+
+$("#back").click(function(){
+
+    result = result.substring(0, result.length - 1);
+    document.getElementById('result').innerHTML = result;
+
+});
