829abb454a940943fce84dba34331c1208d20fdb
vitoxint <vitoxint@gmail.com>
2023-05-02 20:20:27 -0400

    evalua ceros

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-let result = '';
+let result ;
+
+$( document ).ready(function() {
+    result = '0';
+    document.getElementById('result').innerHTML = result;
+});
 
 function addCharacter(character) {
+
+    if( !!!result ){
+        result = '0';
+        console.log(result);
+    }
+    console.log(result);
     result += character;
     document.getElementById('result').innerHTML = result;
 }
@@ -10,17 +21,21 @@ $("#calculate").click(function(){
 
     var final = result.substring(result.length -1 );
 
-    if( isNaN( parseInt(final))){
+    if( isNaN( parseFloat(final))){
         return 0;
     }
 
 
-    result = eval(result);
+    result = parseFloat(eval(result).toFixed(6));
     document.getElementById('result').innerHTML = result;
 
 });
 
 $(".actual").click(function(){
+
+    if( result === '0' ){
+        result = '';
+    }
 
     result += this.value;
     document.getElementById('result').innerHTML = result;
@@ -30,13 +45,24 @@ $(".actual").click(function(){
 
 
 $("#clear").click( function() {
-    result = '';
-    document.getElementById('result').innerHTML = '';
+    result = '0';
+    document.getElementById('result').innerHTML = result;
 });
+
+
+
 
 $("#back").click(function(){
 
-    result = result.substring(0, result.length - 1);
+    if( result.length == 1 ){
+
+        result = 0;
+
+    }else{
+        result = result.substring(0, result.length - 1);
+    }
+
+    //result = result.substring(0, result.length - 1);
     document.getElementById('result').innerHTML = result;
 
 });
